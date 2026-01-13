dfe421f59153fa25ad7dd080d5fea499dc3a3c63
Smith Bhavsar <31366475+smithbhavsar@users.noreply.github.com>
2026-01-13 13:51:23 +0530

    Add skills to players and update captain data
    Updated player and captain data structures to include skills and modified player addition logic.

--- app.js
+++ app.js
@@ -37,39 +37,65 @@ app.use(cors({
 app.use(express.json());
 
 let players = [
-  { name: "Akshat Shah", house: "C/401", age: 16, mobile: "9106729889", baseValue: 0 },
-  { name: "Hetal Thakkar", house: "A/101", age: 49, mobile: "9825264485", baseValue: 0 },
-  { name: "Smith Bhavsar", house: "A/502", age: 27, mobile: "9662727571", baseValue: 0 },
-  { name: "Dr Samir Savaliya", house: "E/103", age: 39, mobile: "9879887775", baseValue: 0 },
-  { name: "Kandarp Patel", house: "A/504", age: 40, mobile: "9824030131", baseValue: 0 },
-  { name: "Heet Shah", house: "D/501", age: 18, mobile: "7041993457", baseValue: 0 },
-  { name: "Deval Ajmera", house: "F/401", age: 37, mobile: "9924926767", baseValue: 0 },
-  { name: "Deep Shah", house: "F/504", age: 32, mobile: "9408292970", baseValue: 0 },
-  { name: "Chirayu Oza", house: "B/202", age: 42, mobile: "9824480084", baseValue: 0 },
-  { name: "CJ", house: "A/203", age: 40, mobile: "9824386738", baseValue: 0 },
-  { name: "Jignesh Shah", house: "D/501", age: 53, mobile: "7041993457", baseValue: 0 },
-  { name: "Hemal Shah", house: "E/102", age: 40, mobile: "8141877711", baseValue: 0 },
-  { name: "Atul Narang", house: "A/502", age: 42, mobile: "8980502109", baseValue: 0 },
-  { name: "Dhruvin Shah", house: "B/404", age: 26, mobile: "12132756231", baseValue: 0 },
-  { name: "Viral Shah", house: "B/403", age: 41, mobile: "9998800045", baseValue: 0 },
-  { name: "Nitya Sanghvi", house: "A/204", age: 17, mobile: "9313410477", baseValue: 0 },
-  { name: "Abhay Chaudhari", house: "C/202", age: 14, mobile: "9727375534", baseValue: 0 },
-  { name: "Rishikesh Darji", house: "E/403", age: 16, mobile: "9974045002", baseValue: 0 },
-  { name: "Jay Darji", house: "E/403", age: 53, mobile: "9974045002", baseValue: 0 },
-  { name: "Deep Shah", house: "D/503", age: 33, mobile: null, baseValue: 0 },
-  { name: "Aryan Shah", house: "C/504", age: 22, mobile: "16026076008", baseValue: 0 },
-  { name: "Manan Bhatt", house: "F/503", age: 36, mobile: "7411286797", baseValue: 0 },
-  { name: "Kushal Patel", house: "B/403", age: 36, mobile: "8238053118", baseValue: 0 },
-  { name: "Karan Shah", house: "D/303", age: 35, mobile: "9033225454", baseValue: 0 },
-  { name: "Sagar Shah", house: "F/504", age: 38, mobile: "9724506844", baseValue: 0 },
-  { name: "Aalap Shah", house: "C/303", age: 44, mobile: "9913957575", baseValue: 0 },
-  { name: "Deval Shah", house: "C/304", age: 32, mobile: "9426548918", baseValue: 0 },
-  { name: "Amit Shah", house: "F/402", age: 50, mobile: "9375202158", baseValue: 0 },
-  { name: "Pradyot Chokshi", house: "D/404", age: 56, mobile: "9825046330", baseValue: 0 },
-  { name: "Harshil Shah", house: "B/302", age: 29, mobile: "9638990743", baseValue: 0 },
-  { name: "Hemant Ajmera", house: "F/401", age: 40, mobile: "9879111783", baseValue: 0 },
-  { name: "Rahil Parikh", house: "C/101", age: 25, mobile: "9106029107", baseValue: 0 },
-  { name: "Smit Shah", house: "E/301", age: 45, mobile: "9825029894", baseValue: 0 },
+  { name: "Amit Gajjar", house: "A/1304", mobile: "9960287599", skills: "Batting", baseValue: 10000 },
+  { name: "Chandan Agrawal", house: "B/401", mobile: "7008564514", skills: "All Rounder", baseValue: 10000 },
+  { name: "Vishal", house: "D/2001", mobile: "9601010009", skills: "All Rounder", baseValue: 10000 },
+  { name: "Mukesh Agrawal", house: "B/303", mobile: "7874441000", skills: "All Rounder", baseValue: 10000 },
+  { name: "Pranav Trivedi", house: "D/902", mobile: "9825804223", skills: "All Rounder", baseValue: 10000 },
+  { name: "Suraj Aggarwal", house: "D/1502", mobile: "9429899905", skills: "All Rounder", baseValue: 10000 },
+  { name: "Dilip Ratwani", house: "B/804", mobile: "9825877480", skills: "Batting", baseValue: 10000 },
+  { name: "Hardev Dodia", house: "D/303", mobile: "9727424512", skills: "All Rounder", baseValue: 10000 },
+  { name: "Anil Sharma", house: "D/1102", mobile: "8141652810", skills: "All Rounder", baseValue: 10000 },
+  { name: "Sachin Batavia", house: "B/1204", mobile: "7874931515", skills: "Batting", baseValue: 10000 },
+  { name: "Parv Gupta", house: "C/1303", mobile: "9711440500", skills: "All Rounder", baseValue: 10000 },
+  { name: "Bharat Goswami", house: "A/903", mobile: "9429206609", skills: "Batting", baseValue: 10000 },
+  { name: "Chirag Akbari", house: "A/1603", mobile: "9033366848", skills: "All Rounder", baseValue: 10000 },
+  { name: "Saurabh Toshniwal", house: "B/901", mobile: "9674737336", skills: "All Rounder", baseValue: 10000 },
+  { name: "Piyush Matai", house: "B/1903", mobile: "9909941144", skills: "Batting", baseValue: 10000 },
+  { name: "Rasik Godhani", house: "C/1202", mobile: "9375936431", skills: "All Rounder", baseValue: 10000 },
+  { name: "Kaushal Kansara", house: "A/1104", mobile: "9979419945", skills: "Batting", baseValue: 10000 },
+  { name: "Jinash Shah", house: "A/904", mobile: "9879614377", skills: "All Rounder", baseValue: 10000 },
+  { name: "Yogesh Thakkar", house: "B/102", mobile: "8980035014", skills: "All Rounder", baseValue: 10000 },
+  { name: "Akash Doultani", house: "B/1603", mobile: "9925265352", skills: "All Rounder", baseValue: 10000 },
+  { name: "Dharmendrasinh Barad", house: "B/1802", mobile: "9712141616", skills: "All Rounder", baseValue: 10000 },
+  { name: "Pankaj Agrawal", house: "B/1902", mobile: "9712969635", skills: "All Rounder", baseValue: 10000 },
+  { name: "Dinesh Bansal", house: "A/901", mobile: "9971948202", skills: "All Rounder", baseValue: 10000 },
+  { name: "Vipul Gajjar", house: "C/1803", mobile: "9328815309", skills: "All Rounder", baseValue: 10000 },
+  { name: "Gautam Modi", house: "B/902", mobile: "9909987705", skills: "All Rounder", baseValue: 10000 },
+  { name: "Tushar Ghelani", house: "D/1004", mobile: "9586299905", skills: "Bowling", baseValue: 10000 },
+  { name: "Ankur Joshi", house: "C/1702", mobile: "8460296800", skills: "All Rounder", baseValue: 10000 },
+  { name: "Manish Gaba", house: "A/1403", mobile: "9826355545", skills: "Bowling", baseValue: 10000 },
+  { name: "Vansh T. Ghelani", house: "D/1004", mobile: "9586199905", skills: "Bowling", baseValue: 10000 },
+  { name: "Atul Narang", house: "C/1904", mobile: "8980502109", skills: "All Rounder", baseValue: 10000 },
+  { name: "Stavan Pandya", house: "D/701", mobile: "9898704606", skills: "All Rounder", baseValue: 10000 },
+  { name: "Rahul Agrawal", house: "D/1704", mobile: "7009550413", skills: "Batting", baseValue: 10000 },
+  { name: "Ashok Sharma", house: "D/801", mobile: "6351081530", skills: "All Rounder", baseValue: 10000 },
+  { name: "Pratik Pujara", house: "C/1003", mobile: "9825938221", skills: "All Rounder", baseValue: 10000 },
+  { name: "Bharat Sajnani", house: "A/101", mobile: "8160252745", skills: "All Rounder", baseValue: 10000 },
+  { name: "Yash Sajnani", house: "A/101", mobile: "9727604943", skills: "Batting", baseValue: 10000 },
+  { name: "Pratik Haria", house: "A/1803", mobile: "9825046163", skills: "Bowling", baseValue: 10000 },
+  { name: "Mohit Jaiswal", house: "C/203", mobile: "8802678330", skills: "All Rounder", baseValue: 10000 },
+  { name: "Apresh Tripathi", house: "D/1501", mobile: "7573032227", skills: "All Rounder", baseValue: 10000 },
+  { name: "Hemanshu Daxini", house: "D/1904", mobile: "9979887899", skills: "Batting", baseValue: 10000 },
+  { name: "Chetan Patel", house: "D/1902", mobile: "9824832541", skills: "All Rounder", baseValue: 10000 },
+  { name: "Rakesh Ramani", house: "D/1802", mobile: "9974013009", skills: "All Rounder", baseValue: 10000 },
+  { name: "Neel Trivedi", house: "D/1002", mobile: "9429881066", skills: "Bowling", baseValue: 10000 },
+  { name: "Abhinav Mammya", house: "B/1704", mobile: "9016901966", skills: "All Rounder", baseValue: 10000 },
+  { name: "Ishan Shah", house: "A/801", mobile: "9099035500", skills: "Batting", baseValue: 10000 },
+  { name: "Vishal Patel", house: "D/2004", mobile: "9825976011", skills: "Bowling", baseValue: 10000 },
+  { name: "Pranjal Pandya", house: "D/102", mobile: "9662008011", skills: "All Rounder", baseValue: 10000 },
+  { name: "Trushant Joshipura", house: "A/401", mobile: "9825007051", skills: "All Rounder", baseValue: 10000 },
+  { name: "Rahul Singhvi", house: "C/1502", mobile: "9825117375", skills: "Batting", baseValue: 10000 },
+  { name: "Samvit Todi", house: "B/1301", mobile: "9925045577", skills: "All Rounder", baseValue: 10000 },
+  { name: "Rajesh Rao", house: "B/403", mobile: "9825017050", skills: "All Rounder", baseValue: 10000 },
+  { name: "Sahaj Goel", house: "D/603", mobile: "9825000873", skills: "All Rounder", baseValue: 10000 },
+  { name: "Viral Gajjar", house: "C/901", mobile: "6351081530", skills: "All Rounder", baseValue: 10000 },
+  { name: "Darsh Gajjar", house: "C/901", mobile: "6351081530", skills: "All Rounder", baseValue: 10000 },
+  { name: "Hemant Arora", house: "C/501", mobile: "9429003982", skills: "All Rounder", baseValue: 10000 },
+  { name: "Shanil Soni", house: "A", mobile: "9998796966", skills: "All Rounder", baseValue: 10000 },
+  { name: "Neev Pujara", house: "C/1003", mobile: "9825938221", skills: "All Rounder", baseValue: 10000 },
+  { name: "Jinesh Shah", house: "A/501", mobile: "9821707072", skills: "All Rounder", baseValue: 10000 },
+  { name: "Prakash Donga", house: null, mobile: "9429052994", skills: "All Rounder", baseValue: 10000 },
 ];
 
 
@@ -78,11 +104,12 @@ let allPlayers = players.map(player => ({ ...player, sold: false }));
 
 // Initialize captains
 let captains = [
-    { id: 1, name: 'Smith Bhavsar', points: 3000, team: [] },
-    { id: 2, name: 'Heet Shah', points: 3000, team: [] },
-    { id: 3, name: 'Deval Ajmera', points: 3000, team: [] },
-    { id: 4, name: 'Rohan Shah', points: 3000, team: [] },
-    { id: 5, name: 'Parth Gurjar', points: 3000, team: [] },
+    { id: 1, name: 'Dilip Ratwani', points: 1000000, team: [] },
+    { id: 2, name: 'Rahul Singhvi', points: 1000000, team: [] },
+    { id: 3, name: 'Tushar Ghelani', points: 1000000, team: [] },
+    { id: 4, name: 'Parv Gupta', points: 1000000, team: [] },
+    { id: 5, name: 'Dinesh Bansal', points: 1000000, team: [] },
+    { id: 6, name: 'Mukesh Agrawal', points: 1000000, team: [] }
 ];
 
 allPlayers = allPlayers.filter(player => !captains.some(captain => captain.name === player.name));
@@ -93,6 +120,8 @@ let randomizedPlayers = players.sort(() => Math.random() - 0.5);
 let currentPlayerIndex = 0;
 let currentPlayer = randomizedPlayers[currentPlayerIndex];
 let passedPlayers = [];
+const BASE_VALUE = 10000;
+const MAX_TEAM_SIZE = 10;
 
 allPlayers = allPlayers.filter(player => !captains.some(captain => captain.name === player.name));
 
@@ -101,12 +130,21 @@ players = players.filter(player => !captains.some(captain => captain.name === pl
 
 // API to add a player
 app.post('/players', (req, res) => {
-    const { name, house, age, mobile } = req.body;
-    const newPlayer = { name, house, age, mobile, sold: false }; // Add sold property
+    const { name, house, skills, mobile } = req.body;
+    const newPlayer = { name, house, skills, mobile, sold: false }; // Add sold property
     players.push(newPlayer);
     allPlayers.push({ ...newPlayer }); // Update allPlayers as well
     res.status(201).json(newPlayer);
 });
+
+function remainingSlots(captain) {
+    return MAX_TEAM_SIZE - captain.team.length;
+}
+
+function getMaxAllowedBid(captain) {
+    const slotsLeft = remainingSlots(captain);
+    return captain.points - (slotsLeft - 1) * BASE_VALUE;
+}
 
 // Get all players (for the players list page)
 app.get('/all-players', (req, res) => {
@@ -118,30 +156,16 @@ app.get('/players', (req, res) => {
     res.json(randomizedPlayers);
 });
 
-// function resetAuction() {
-//     randomizedPlayers = players.sort(() => Math.random() - 0.5); // Randomize players again
-//     currentPlayerIndex = 0;
-//     currentPlayer = randomizedPlayers[currentPlayerIndex];
-//     passedPlayers = [];
-//     captains.forEach(captain => {
-//         captain.team = [];
-//         captain.points = 1500; // Reset captain points
-//     });
-
-//     // Reset all players to available (sold: false)
-//     allPlayers.forEach(player => {
-//         player.sold = false; // Mark all players as available
-//     });
-
-//     io.emit('auctionStart', { randomizedPlayers, captains, currentPlayer });
-//     io.emit('bidUpdate', { allPlayers }); // Emit updated allPlayers to clients
-// }
-
-
 io.on('connection', (socket) => {
     console.log('A user connected');
 
     socket.emit('auctionStart', { randomizedPlayers, captains, currentPlayer });
+
+    captains.forEach(captain => {
+        socket.emit('bidLimits', {
+            captainId: captain.id,
+            maxBid: getMaxAllowedBid(captain)
+        });
 
     socket.on('getAuctionData', () => {
         // Send the auction data to the client when requested
@@ -152,35 +176,60 @@ io.on('connection', (socket) => {
         });
     });
 
-    // socket.on('resetAuction', () => {
-    //     resetAuction(); // Reset the auction and emit the start data again
-    // });
-
     socket.on('placeBid', (bid, captainId) => {
-        const captain = captains.find(c => c.id === captainId);
+    const captain = captains.find(c => c.id === captainId);
 
-        if (captain.team.length >= 6) {
-            socket.emit('errorMessage', 'You can only buy 6 players');
-            return;
-          }
+    const slotsLeft = remainingSlots(captain);
 
-        if (bid > captain.points) {
-            socket.emit('errorMessage', 'Not enough points to place this bid!');
-        } else {
-            captain.points -= bid;
+    // Team full check
+    if (slotsLeft <= 0) {
+        socket.emit('errorMessage', 'Your team is already full');
+        return;
+    }
 
-            currentPlayer.sold = true;
-            const allPlayersIndex = allPlayers.findIndex(p => p.name === currentPlayer.name);
-            if (allPlayersIndex !== -1) {
-                allPlayers[allPlayersIndex].sold = true;
-            }
+    // Minimum bid enforcement
+    if (bid < BASE_VALUE) {
+        socket.emit(
+            'errorMessage',
+            `Minimum bid is ₹${BASE_VALUE}`
+        );
+        return;
+    }
 
-            captain.team.push({ player: currentPlayer, bid });
-            io.emit('bidUpdate', { captains, currentPlayer, allPlayers });
+    const maxAllowedBid = getMaxAllowedBid(captain);
 
-            moveToNextPlayer();
-        }
-    });  
+    // Core constraint logic
+    if (bid > maxAllowedBid) {
+        socket.emit(
+            'errorMessage',
+            `You can bid max ₹${maxAllowedBid}. You must reserve ₹${BASE_VALUE * (slotsLeft - 1)} for remaining players`
+        );
+        return;
+    }
+
+    // Final balance safety
+    if (bid > captain.points) {
+        socket.emit('errorMessage', 'Not enough points');
+        return;
+    }
+
+    // Deduct points
+    captain.points -= bid;
+
+    // Mark sold
+    currentPlayer.sold = true;
+    const allPlayersIndex = allPlayers.findIndex(p => p.name === currentPlayer.name);
+    if (allPlayersIndex !== -1) {
+        allPlayers[allPlayersIndex].sold = true;
+    }
+
+    // Add to team
+    captain.team.push({ player: currentPlayer, bid });
+
+    io.emit('bidUpdate', { captains, currentPlayer, allPlayers });
+
+    moveToNextPlayer();
+});
 
     socket.on('passPlayer', () => {
         console.log('Player passed:', currentPlayer.name);
